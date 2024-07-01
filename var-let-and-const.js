5775fa369b32babd3af3c9dca67b131a5ff31c74
slam814 <abdussalam6111997@gmail.com>
2024-07-01 11:06:59 +0600

    i clear var let const

---
+++ var-let-and-const.js
@@ -0,0 +1,15 @@
+var mySalary = 5000;
+
+mySalary = 200;
+var mySalary = 70000;
+console.log(mySalary);
+
+let money = 50;
+ money = 30;
+//  let money = 45;
+ console.log(money);
+
+
+ const myMoney = 80;
+//  myMoney = 90;
+ console.log(myMoney);
