43051e6c11c2a887a892c95bd7dd8ac5c89ecaec
slam814 <abdussalam6111997@gmail.com>
2024-07-02 09:41:58 +0600

    i added some function

--- var-let-and-const.js
+++ var-let-and-const.js
@@ -11,5 +11,23 @@ let money = 50;
 
 
  const myMoney = 80;
-//  myMoney = 90;
- console.log(myMoney);
+// //  myMoney = 90;
+//  console.log(myMoney);
+
+const rech = myMoney + 500;
+console.log(rech);
+
+
+const numbers = [23, 4, 23, 12, 56];
+// numbers = [4, 5, 66, 77, 90]; // you are not reAssignment aray 
+numbers[1] = 88; // but you are chenge are aney index value
+numbers.push(8, 9, 55, 77, 100, 'push'); // but you are chenge are push
+console.log(numbers);
+
+// object 
+const student = {
+    name: 'moyna pakhi',
+    class: 12
+}
+student.name = 'kokil konthi'
+console.log(student);
